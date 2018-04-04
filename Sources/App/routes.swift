d9077fca99988403baf842e7fa4c0b3cc04a485b
Eduardo González <egonzalez@iphonedroid.com>
2018-04-04 15:03:04 +0200

    Accepting data

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,7 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
+
     router.get("hello") { req in
         return "Hello, world!"
     }
@@ -16,13 +16,22 @@ public func routes(_ router: Router) throws {
         let name = try req.parameter(String.self)
         return "Hello \(name)"
     }
-
     
-    
+    router.post(InfoData.self, at: "info") { req, data -> InfoResponse in
+        return InfoResponse(request: data)
+    }
     
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
